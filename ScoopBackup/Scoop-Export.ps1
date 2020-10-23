53716b21821e746d4e8d9cca632109652f9d2458
Cologler <skyoflw@gmail.com>
2020-10-23 10:55:37 +0800

    feat: export no bucket apps

--- ScoopBackup/Scoop-Export.ps1
+++ ScoopBackup/Scoop-Export.ps1
@@ -3,17 +3,25 @@
 function ScoopExport {
     $buckets = Get-Buckets
     $apps = Get-UserScopeApps
+    $noBucketApps = @{}
 
     $apps.Values | ForEach-Object {
         $app = $_
-        if ($buckets.ContainsKey($app.Bucket)) {
-            # if user remove bucket, unable to backup
-            $bucket = $buckets[$app.Bucket]
-            if (!$bucket.ContainsKey("Apps")) {
-                $bucket["Apps"] = @{}
+        if ($app.Bucket) {
+            if ($buckets.ContainsKey($app.Bucket)) {
+                # if user remove bucket, unable to backup
+                $bucket = $buckets[$app.Bucket]
+                if (!$bucket.ContainsKey("Apps")) {
+                    $bucket["Apps"] = @{}
+                }
+                $bucket.Apps[$app.Name] = @{
+                    #Version = $app.Version # scoop did not support select version
+                    Arch = $app.Arch
+                }
             }
-            $bucket.Apps[$app.Name] = @{
-                #Version = $app.Version # scoop did not support select version
+        } elseif ($app.Url) {
+            $noBucketApps[$app.Name] = @{
+                Url = $app.Url
                 Arch = $app.Arch
             }
         }
@@ -29,6 +37,9 @@ function ScoopExport {
         if (!$rv.Buckets.ContainsKey($url)) {
             $rv.Buckets[$url] = $bucket
         }
+    }
+    if ($noBucketApps.Count -gt 0) {
+        $rv.NoBucketApps = $noBucketApps
     }
 
     return $rv | ConvertTo-Json -Depth 10
